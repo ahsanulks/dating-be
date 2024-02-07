77332b6194be7249e9658c60308103852883c44c
ahsanulks <ahsanulkh996@gmail.com>
2024-02-07 06:55:39 +0000

    feat: add migration for create user

---
+++ migrations/20240207060447_create_user_table.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users
+(
+    id              BIGSERIAL    PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    username        VARCHAR(255) NOT NULL UNIQUE NOT NULL,
+    password        VARCHAR(255) NOT NULL,
+    phone_number    VARCHAR(15)  UNIQUE NOT NULL,
+    gender          VARCHAR(10)  NOT NULL,     
+    created_at      TIMESTAMPTZ  DEFAULT NOW(),
+    updated_at      TIMESTAMPTZ  DEFAULT NOW()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
